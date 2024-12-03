8e9cd0f64957785e67035e77e97f3634f5feca98
jaelhood <jaehyung.lim@robinhood.com>
2024-12-03 16:27:57 -0500

    Update startup.sh

--- environment/startup.sh
+++ environment/startup.sh
@@ -5,6 +5,7 @@ cd
 cd /Users/jaehyung.lim/Documents/GitHub
 
 rm -rf aa_template 
+git clone https://github.com/jaelhood/aa_template
 conda env create -f aa_template/environment/hoodie-credit.yaml
 
 source activate hoodie-credit
